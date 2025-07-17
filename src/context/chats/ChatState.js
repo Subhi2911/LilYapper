6fbe44aafb749bdafa2ec3b7360008aaa40aa577
Subhasmita Mishra <msubhasmita29@gmail.com>
2025-07-18 01:03:19 +0530

    commit

---
+++ src/context/chats/ChatState.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import ChatContext from "./ChatContext";
+
+const ChatState = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChatState
